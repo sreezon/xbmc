29f48044ee5b785a6b4400a082c6f41368f3a42f
Ali Kayes <alikayes@berkeley.edu>
2025-04-05 11:49:54 -0700

    Revert "add fel only audio delay"
    This reverts commit 1f36ccb8d557576847ff79adb65fcd33e1781b06.
    
    revert commit

--- xbmc/settings/AdvancedSettings.h
+++ xbmc/settings/AdvancedSettings.h
@@ -105,7 +105,6 @@ struct PassthroughAudioLatency
   CAEStreamInfo::DataType type;
 
   int delay;
-  bool fel_only = false;
 };
 
 typedef std::vector<TVShowRegexp> SETTINGS_TVSHOWLIST;
@@ -398,12 +397,9 @@ class CAdvancedSettings : public ISettingCallback, public ISettingsHandler
     float m_videoDecoderMinimumBuffer;
     float m_videoDecoderMinimumStreamBuffer;
 
-    bool HasFELData() const;
-    void SetHasFELData(bool has_fel);
 
   private:
     void Initialize();
     void Clear();
     void SetExtraArtwork(const TiXmlElement* arttypes, std::vector<std::string>& artworkMap);
-    bool m_hasFELData = false;
 };
