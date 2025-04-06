4513772fde3dfff0d4cc32577375a013b36af166
Ali I. Kayes <alikayes@berkeley.edu>
2025-04-05 21:31:33 -0700

    fix clock

--- xbmc/cores/VideoPlayer/VideoPlayerAudio.cpp
+++ xbmc/cores/VideoPlayer/VideoPlayerAudio.cpp
@@ -495,7 +495,7 @@ bool CVideoPlayerAudio::ProcessDecoderOutput(DVDAudioFrame &audioframe)
       
       // Track time since last sync reset
       static double lastSyncResetTime = 0;
-      double currentTime = m_dvdClock.GetAbsoluteClock();
+      double currentTime = m_pClock->GetAbsoluteClock();
       bool timeForPeriodicReset = (currentTime - lastSyncResetTime) > 60000; // Force resync every 60 seconds
       
       // Update if status has changed or it's time for a periodic reset
